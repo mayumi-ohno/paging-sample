cd7402917f6ee10b0248e386b32d98580ba9326c
伊賀将之 <igamasayuki@igamasayukinoMacBook-puro.local>
2019-02-25 12:34:58 +0900

    バグフィックスアンサーfirst commit

---
+++ src/main/java/jp/co/sample/emp_management/service/AdministratorService.java
@@ -0,0 +1,47 @@
+package jp.co.sample.emp_management.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.sample.emp_management.domain.Administrator;
+import jp.co.sample.emp_management.repository.AdministratorRepository;
+
+/**
+ * 管理者情報を操作するサービス.
+ * 
+ * @author igamasayuki
+ *
+ */
+@Service
+@Transactional
+public class AdministratorService {
+	
+	@Autowired
+	private AdministratorRepository administratorRepository;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	/**
+	 * 管理者情報を登録します.
+	 * 
+	 * @param administrator　管理者情報
+	 */
+	public void insert(Administrator administrator) {
+		// パスワード暗号化
+		administrator.setPassword(passwordEncoder.encode(administrator.getPassword()));
+		administratorRepository.insert(administrator);
+	}
+	
+	/**
+	 * ログインをします.
+	 * @param mailAddress メールアドレス
+	 * @param password パスワード
+	 * @return 管理者情報　存在しない場合はnullが返ります
+	 */
+	public Administrator login(String mailAddress, String passward) {
+		Administrator administrator = administratorRepository.findByMailAddressAndPassward(mailAddress, passward);
+		return administrator;
+	}
+}
