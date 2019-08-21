85431192385a5d01a9c1ac39b063787bddf258b7
伊賀将之 <igamasayuki@igamasayukinoMacBook-puro.local>
2019-08-21 16:09:31 +0900

    spring securityのバグに対応

--- src/main/java/jp/co/sample/emp_management/SecurityConfig.java
+++ src/main/java/jp/co/sample/emp_management/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.loginPage("/") // ログイン画面に遷移させるパス(ログイン認証が必要なパスを指定してかつログインされていないとこのパスに遷移される)
 			.loginProcessingUrl("/login") // ログインボタンを押した際に遷移させるパス(ここに遷移させれば自動的にログインが行われる)
 			.failureUrl("/?error=true") //ログイン失敗に遷移させるパス
-			.defaultSuccessUrl("/employee/showList", true) // 第1引数:デフォルトでログイン成功時に遷移させるパス
+			.defaultSuccessUrl("/employee/showList", false) // 第1引数:デフォルトでログイン成功時に遷移させるパス
 			                                        // 第2引数: true :認証後常に第1引数のパスに遷移 
 			                                        //         false:認証されてなくて一度ログイン画面に飛ばされてもログインしたら指定したURLに遷移
 			.usernameParameter("mailAddress") // 認証時に使用するユーザ名のリクエストパラメータ名(今回はメールアドレスを使用)
